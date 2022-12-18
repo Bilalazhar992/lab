407457cacec9ccd730b66fd379a687d09f1f13b2
Bilalazhar992 <120160350+Bilalazhar992@users.noreply.github.com>
2022-12-18 19:37:51 +0500

    Add files via upload

---
+++ goat.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+main ()
+{
+cout<<"  ------------------------------------"<<endl;
+
+cout<<"              ^ _ ^           "<<endl;   
+cout<<"              (@ @)\\___________           "<<endl;
+cout<<"                                                "<<endl;
+cout<<"              (___)\\           )\\//\\  "<<endl;
+cout<<"                                                   "<<endl;
+cout<<"                   || ------w  |     "<<endl;
+cout<<"                   ||         ||  "<<endl;
+}
